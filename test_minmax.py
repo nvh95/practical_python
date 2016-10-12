561e2655aa7cfc9016169cf188d9650d992d793d
Hung Nguyen <nguyenviethunghl00@gmail.com>
2016-10-12 12:23:01 +0700

    test_minmax

--- test_minmax.py
+++ test_minmax.py
@@ -6,8 +6,5 @@ def test_maxmin():
     assert find_two_smallest([7,2,3,4,4,5,]) == (1,2), "Fails long lists"
 
 if __name__ == '__main__': 
-    print 1
-    print find_two_smallest([1,2]) ==(1,2)
-    print 10
     nose.runmodule()
     
